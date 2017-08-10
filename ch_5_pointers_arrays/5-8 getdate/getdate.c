a719adf9d865cfbc9d65eef1a874d3f9501e11e5
Jing Lin <Jinglin82@gmail.com>
2017-08-10 03:24:55 -0700

    5-8 error checking with assert

--- ch_5_pointers_arrays/5-8 getdate/getdate.c
+++ ch_5_pointers_arrays/5-8 getdate/getdate.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 static char daytab[2][13] = {
 	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
@@ -19,6 +21,11 @@ int day_of_year(int year, int month, int day) {
 
 	leap = (year % 4 == 0 && year % 100 != 0) 
 			|| year % 400 == 0;
+
+	assert(year > 0);
+	assert(month > 0 && month <= 12);
+	assert(day > 0 && day <= daytab[leap][month]);
+
 	for (i = 1; i < month; i++)
 		day += daytab[leap][i];
 	return day;
@@ -26,6 +33,9 @@ int day_of_year(int year, int month, int day) {
 
 void month_day(int year, int yearday, int *pmonth, int *pday) {
 	int i, leap;
+
+	assert(year > 0);
+	assert(yearday > 0 && yearday <= 365);
 
 	leap = (year % 4 == 0 && year % 100 != 0)
 		    || year % 400 == 0;
