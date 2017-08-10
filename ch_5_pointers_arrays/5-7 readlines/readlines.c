f636633a27797a22e51e0ddd9e853c83794f40cb
Jing Lin <Jinglin@mit.edu>
2017-08-10 01:10:20 -0700

    5-7 Incorporated quicksort

---
+++ ch_5_pointers_arrays/5-7 readlines/readlines.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+
+char *lineptr[MAXLINES]
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void q_sort(char *lineptr[], int left, int right);
+
+int main() {
+	int nlines;
+
+	if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+		q_sort(lineptr, 0, nlines - 1);
+		writelines(lineptr, nlines);
+		return 0;
+	} else {
+		printf("error: input too big to sort\n");
+		return 1;
+	}
+}
+
+#define MAXLEN 1000
+int get_line(char *, int);
+int min(int, int);
+char *alloc(int);
+
+int get_line(char s[], int lim) {
+	int c, i;
+	for(i = 0; (c = getchar()) != EOF && c != '\n'; ++i) {
+		if (i < lim - 1)
+			s[i] = c;
+	}
+	if ((c == '\n') && i < lim - 1) {
+		s[i] = c;
+		++i;
+	}
+	s[min(i, lim - 1)] = '\0';
+	return i;
+}
+
+int min(int a, int b) {
+	if (a <= b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+int readlines(char *lineptr[], int maxlines) {
+	int len, nlines;
+	char *p, line[MAXLEN];
+
+	nlines = 0;
+	while ((len = get_line(line, MAXLEN)) > 0) {
+		if (nlines >= maxlines || (p = alloc(len)) == NULL)
+			return -1;
+		else {
+			line[len - 1] = '\0';
+			strcpy(p, line);
+			lineptr[nlines++] = p;
+		}
+	}
+	return nlines;
+} 
+
+void writelines(char *lineptr[], int nlines) {
+	// int i;
+	// for (i = 0; i < nlines; i++) 
+	// 	printf("%s\n", lineptr[i]);
+	while (nlines-- > 0) 
+		printf("%s\n", *lineptr++);
+}
+
+void q_sort(char *v[], int left, int right) {
+	int i, last;
+	void swap(char *v[], int i, int j);
+
+	if (left >= right)
+		return;
+	swap(v, left, (left + right) / 2);
+	last = left;
+	for (i = left + 1; i <= right; i++) {
+		if (strcmp(v[i], v[left]) < 0)
+			swap(v, ++last, i);
+	}
+	swap(v, left, last);
+	q_sort(v, left, last - 1);
+	q_sort(v, last + 1, right);
+}
+
+void swap(char *v[], int i, int j) {
+	char *temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
