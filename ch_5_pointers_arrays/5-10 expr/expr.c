4275dae8b027a7fb4b274fb143e40aa47ce539fc
Jing Lin <Jinglin82@gmail.com>
2017-08-11 02:09:00 -0700

    5-10 done

--- ch_5_pointers_arrays/5-10 expr/expr.c
+++ ch_5_pointers_arrays/5-10 expr/expr.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define ELEMENTS 500
 
@@ -17,7 +18,7 @@ void push(double f) {
 
 double pop(void) {
 	if (sp > 0)
-		return stack[sp--];
+		return stack[--sp];
 	else {
 		printf("empty stack\n");
 		return 0;
@@ -25,9 +26,35 @@ double pop(void) {
 }
 
 int main(int argc, char *argv[]) {
+	int c;
+	char *element;
+	double op2;
 
-	while (--argc > 0) {
+	argv++;
 
+	while (argc-- > 1) {
+		element = argv[0];
+		switch (c = *argv[0]) {
+			case '+':
+				push(pop() + pop());
+				break;
+			case '*':
+				push(pop() * pop());
+				break;
+			case '-':
+				op2 = pop();
+				push(pop() - op2);
+				break;
+			case '/':
+				op2 = pop();
+				push(pop() / op2);
+				break;
+			default:
+				push(atof(element));
+				break;
+		}
+		argv++;
 	}
+	printf("The calculated value was %.8g\n", pop());
 }
 
